a1169e2405911c4d317f6a6902fb65de71323cc6
hachim <hachim.amadi@my-digital-school.org>
2024-03-28 09:47:21 +0100

    begining of map func

---
+++ src/component/Map/Map.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const Map = () => {
+  const [map, setMap] = useState(null);
+
+  function getCurrentPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setMap({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+          console.log("Geolocation: ", map);
+          alert("Latitude: " + map.latitude + ", Longitude: " + map.longitude);
+        },
+        (error) => {
+          console.error("Error getting current position:", error);
+            alert("ggggg" , error)
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={getCurrentPosition}>Get Loction</button>
+      {/* Afficher la carte si la variable map est définie */}
+      {map && (
+        <div>
+          Latitude: {map.latitude}, Longitude: {map.longitude}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Map;
---
+++ src/page/map/Geo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from '../../component/Map/Map';
+
+const Geo = () => {
+    // Code pour afficher la carte ici
+
+    return (
+        <div>
+            <Map />
+        </div>
+    );
+};
+
+export default Map;
