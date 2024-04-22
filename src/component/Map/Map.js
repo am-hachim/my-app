4e2ec23380a17f33717383fcf1ad5b541b7b00ba
hachim <hachim.amadi@my-digital-school.org>
2024-04-22 10:08:13 +0200

    permision

--- src/component/Map/Map.js
+++ src/component/Map/Map.js
@@ -5,22 +5,36 @@ const Map = () => {
 
   function getCurrentPosition() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setMap({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          });
-          console.log("Geolocation: ", map);
-          alert("Latitude: " + map.latitude + ", Longitude: " + map.longitude);
-        },
-        (error) => {
-          console.error("Error getting current position:", error);
-            alert("ggggg" , error)
-        }
-      );
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            // If permission is already granted, get the current position
+            navigator.geolocation.getCurrentPosition(success, error);
+          } else if (result.state === "prompt") {
+            // If permission is not yet granted, request permission and then get the current position
+            navigator.geolocation.getCurrentPosition(success, error);
+          } else if (result.state === "denied") {
+            // If permission is denied, handle accordingly
+            console.error("Permission to access location was denied");
+          }
+        });
     } else {
       console.log("Geolocation is not supported by this browser.");
+    }
+  
+    function success(position) {
+      setMap({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+      console.log("Geolocation: ", map);
+      alert("Latitude: " + map.latitude + ", Longitude: " + map.longitude);
+    }
+  
+    function error(err) {
+      console.error("Error getting current position:", err);
+      alert("ggggg", err);
     }
   }
 
