cf1962bdb24038d567a1ca1257ef5229fbe57e1d
hachim <hachim.amadi@my-digital-school.org>
2024-04-22 16:16:21 +0200

    aghhhhhhhhhhhhhhhhhhhhhh

--- src/component/Map/Map.js
+++ src/component/Map/Map.js
@@ -40,7 +40,7 @@ const Map = () => {
 
   return (
     <div>
-      <button onClick={getCurrentPosition}>Get Loction mon negro</button>
+      <button onClick={getCurrentPosition}>Get Ltion mon negro</button>
       {/* Afficher la carte si la variable map est définie */}
       {map && (
         <div>
