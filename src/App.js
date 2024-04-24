992a63216ab061dff753e168803941d31b373576
hachim <hachim.amadi@my-digital-school.org>
2024-04-24 15:18:40 +0200

    zdefgb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Notification } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './App.css';
 
